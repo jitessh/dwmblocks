bf00c90ce64192124a2a955370a5a1930bed36ca
Jitesh <85620326+voidstarsh@users.noreply.github.com>
2021-07-29 21:24:58 +0530

    Update config.h

--- config.h
+++ config.h
@@ -1,11 +1,14 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+/* modify this file to change what commands output to your statusbar, and recompile using the make command. */
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+static const Block blocks[] = {
+    /* Icon         Command                 Interval    Signal */
+    { "",           "sb-ram",               60,         0       },
+    { "",           "sb-datetime",          10,         0       },
+    { "",           "sb-volume",            0,          10      },
+    { "",           "sb-battery",           60,         0       },
+    { "",           "sb-internet",          60,         0       },
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+/* sets delimeter between status commands. NULL character ('\0') means no delimeter. */
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
